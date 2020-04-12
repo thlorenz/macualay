2ccf4f83299b3ea93d28a7249b54fa403c282e34
Thorsten Lorenz <thlorenz@gmx.de>
2020-04-11 23:33:03 -0600

    app: rendering super simple table

--- modules/app/src/components/Table.tsx
+++ modules/app/src/components/Table.tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import { AppController } from '../logic/app-controller'
 import { BirdDataRow } from '@modules/core'
 
-export function Table() {
-  const results: BirdDataRow[] = AppController.instance.useQueryResult()
-  const stringified = JSON.stringify(results, null, 2)
+function columnsFromData(data: BirdDataRow[]) {
+  if (data.length === 0) return []
+  const row = data[0]
+  const cols: string[] = Object.keys(row)
+  return cols
+}
+
+function renderRow(row: BirdDataRow) {
+  const cells = Object.values(row).map(x => <td>{x}</td>)
   return (
-    <div>
-      {stringified}
-    </div>
+    <tr>
+      {cells}
+    </tr>
+  )
+}
+
+export function Table() {
+  const data: BirdDataRow[] = AppController.instance.useQueryResult()
+  const columns: string[] = columnsFromData(data)
+  return (
+    <table>
+      <thead>
+      {columns.map(col => <th>{col}</th>)}
+      </thead>
+      <tbody>
+      {data.map(renderRow)}
+      </tbody>
+    </table>
   )
 }
